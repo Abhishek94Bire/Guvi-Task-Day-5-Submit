604f7a187492a5ff53309eb529c9fa3b2c380a71
Abhishek94Bire <137614575+Abhishek94Bire@users.noreply.github.com>
2023-12-10 20:12:54 +0530

    Update JsonResume.js

--- JsonResume.js
+++ JsonResume.js
@@ -1,63 +1,62 @@
 let myResume = { 
     basics:
     {
-      name: "Hemamalini",
-      email: "123@gmail.com",
+      name: "AbhiBire",
+      email: "AbhiBire94@gmail.com",
       mobile: "123456789"
     },
    profiles:
     {
-        linkedin: "https://www.linkedin.com/in/hemamalini-kamaraj-093271a1",
-        github: "https://github.com/Hemamalini-Kamaraj"
+        linkedin: "https://www.linkedin.com/in/abhishek-bire-77357a257/",
+        github: "https://github.com/Abhishek94Bire"
     },
    education:
     [ {
-        institutionName: "Info Institute of Engineering",
-        degree: "BE-ECE",
+        institutionName: "Rajiv Gandhi Clg of Engineering",
+        degree: "BE-Mechanical",
         year: "2014-2018",
         marks: "7 cgpa",
-        city: "Coimbatore"
+        city: "Nagpur"
     },
       {
-        institutionName: "P.S.G.R Krishnammal Hr.Sec School",
+        institutionName: "A.C.S Hr.Sec School",
         degree: "HSC",
         year: "2012-2014",
         marks: "79%",
-        city: "Coimbatore"
+        city: "Nagpur"
       },
       {
-        institutionName: "T.R.A. Hr.Sec School",
-        degree: "SSLC",
+        institutionName: "BBCHS Hr.Sec School",
+        degree: "SSC",
         year: "2010-2012",
         marks: "90%",
-        city: "Coimbatore"
+        city: "Nagpur"
       } 
     ],
    workExperience:
    [
-    { companyName: "Urjanet Energy Solutions",
-      designation: "Software Developer",
+    { companyName: "Premier Plant And Service",
+      designation: "Purchase Engineeer",
       startDate: "11-02-2019",
       endDate: "15-10-2021",
-      projectSummary: "Automated big energy data",
       rolesandResponsibilities:
       {
-         point1: "Development, Enhancement",
-         point2: "Doing code review",
-         point3: "Deployment and Root Cause Analysis"
+         point1: "Material Inward",
+         point2: "Material Outward",
+         point3: "Daily Stock Check",
       } 
     },
     {
-      companyName: "Wintra Engineering Pvt Ltd",
-      designation: "Junior Engineer",
+      companyName: "Speed Wheelz",
+      designation: "PurchaseEngineer",
       startDate: "10-06-2018",
       endDate: "20-01-2019",
       projectSummary: "Bus Destination Display System",
       rolesandResponsibilities:
       {
-        point1: "Programming",
-        point2: "Soldering",
-        point3: "Mentoring",
+         point1: "Material Inward",
+         point2: "Material Outward",
+         point3: "Daily Stock Check",
       }
     }
     ], 
@@ -75,7 +74,7 @@ let myResume = {
     Achievements:
     {
         point1: "Completed traing on Raspberry pi",
-        point2: "Participated in International Robotic Competion held in IIT Chennai",
+        point2: "Participated in International Robotic Competion held in IIT Mumbai",
         point3: "Completed 5 days internship in Microprocessor 7 MicroController"
     },
     "Extra Curricular Activities":
@@ -91,20 +90,20 @@ let myResume = {
     },
    languages:
     [ 
-      { language: "Tamil", proficiency: "Native" },
-      { language: "English", proficiency: "Fluent" } 
+      { language: "Marathi", proficiency: "Fluent" },
+      { language: "English", proficiency: "Native" } 
     ],
    personalInformation:
     { 
-      name: "Hemamalini",
-      maritalStatus: "Married",
+      name: "AbhishekBire",
+      maritalStatus: "Single",
       age: "26",
       address:
        { doorNo: "29",
-         street: "Maruthamalai goundar Street",
-         city: "Coimbatore",
-         pincode: "641006",
-         State: "Tamilnadu" } 
+         street: "Sarkar Street",
+         city: "Nagpur",
+         pincode: "442401",
+         State: "M.H" } 
     } 
     }
 
